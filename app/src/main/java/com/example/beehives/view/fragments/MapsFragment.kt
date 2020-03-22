7757c3a209d636a5c607a310f557710d05d78f41
Yevheniy <Djek2025@gmail.com>
2020-03-23 01:21:07 +0200

    Map displays all apiaries from cloud

--- app/src/main/java/com/example/beehives/view/fragments/MapsFragment.kt
+++ app/src/main/java/com/example/beehives/view/fragments/MapsFragment.kt
@@ -1,26 +1,40 @@
 package com.example.beehives.view.fragments
 
-import androidx.fragment.app.Fragment
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.beehives.R
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MapsFragment : Fragment() {
 
+    fun getApiaries(gMap: GoogleMap) {
+        val database = FirebaseFirestore.getInstance()
+        database.collection("Apiaries").get().addOnSuccessListener {
+            it.documents.forEach {
+                val temp = it.getGeoPoint("Coords")
+                val latlng = LatLng(temp!!.latitude, temp.longitude)
+                gMap.addMarker(MarkerOptions().position(latlng).title("Apiary"))
+                gMap.addCircle(CircleOptions().center(latlng).radius(3000.00))
+            }
+        }
+    }
+
     private val callback = OnMapReadyCallback { googleMap ->
         val cherkasy = LatLng(49.4285, 32.0620)
-        googleMap.addMarker(MarkerOptions().position(cherkasy).title("Marker in Cherkasy"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cherkasy, 10f))
+        googleMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cherkasy, 6f))
+        getApiaries(googleMap)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
